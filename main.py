656890999e722b70278adcde2fbc30feef56700f
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-02-20 18:22:17 -0600

    feat: update main.py

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 def find_left_handed_seats(table):
+    # --- To check the asigned seat
+    # Init count in 0
     count = 0
     for i in range(4):
-        if table[0][0] == "U":
-            table[0][0] = "L"
-            count += 1
-        if table[0][i] == "U" and i != 4 and table[0][i+1] != "R":
-            table[0][i] = "L"
-            count += 1
-        if table[1][i] == "U" and i != 0 and table[1][i-1] != "R":
-            table[1][i] = "L"
-            count += 1
+        # Borderline case first seat
+        if i == 0:
+            if table[0][i] == "U" and table[0][i+1] != "R":count += 1   # ---table[0][i] = "L"
+            if table[1][i] == "U" :count += 1                           # ---table[1][i] = "L"
+                
+        # Borderline Case last seat
+        elif i == len(table[0])-1:
+            if table[0][i] == "U":count += 1                            # ---table[0][i] = "L"
+            if table[1][i] == "U" and table[1][i-1] != "R":count += 1   # ---table[1][i] = "L" 
 
-
-    return table
+        # Other cases
+        else:
+            if table[0][i] == "U" and table[0][i+1] != "R":count += 1   # ---table[0][i] = "L"
+            if table[1][i] == "U" and table[1][i-1] != "R":count += 1   # ---table[1][i] = "L"
+                
+    return count
 
 if __name__ == '__main__':
     print(find_left_handed_seats([["U", "R", "U", "L"], ["U", "R", "R", "R"]]))
     print('--------')
-    #print(find_left_handed_seats([["U", "U", "U", "U"], ["U", "U", "U", "U"]]))
+    print(find_left_handed_seats([["U", "U", "U", "U"], ["U", "U", "U", "U"]]))
     print('--------')
-    #print(find_left_handed_seats([["U", "R", "U", "R"], ["L", "R", "R", "U"]]))
+    print(find_left_handed_seats([["U", "R", "U", "R"], ["L", "R", "R", "U"]]))
     print('--------')
-    #print(find_left_handed_seats([["L", "U", "R", "R"], ["L", "U", "R", "R"]]))
+    print(find_left_handed_seats([["L", "U", "R", "R"], ["L", "U", "R", "R"]]))
     print('--------')
-    #print(find_left_handed_seats([["U", "R", "U", "U"], ["U", "U", "L", "U"]]))
+    print(find_left_handed_seats([["U", "R", "U", "U"], ["U", "U", "L", "U"]]))
     print('--------')
